2e84a96bd05dbdd0adad22e0eaf41eeb49b0db1b
Vallirie <Tellnobodywants1216@gmail.com>
2025-12-31 02:38:19 +0800

    Trial

---
+++ Cute.java
@@ -0,0 +1,8 @@
+class Cute{
+    public static void main(String[] args) {
+        //HAHAHAH
+            System.out.println("Hello World!!");
+            System.out.println("Testing!!");
+            System.out.println("Trial is here!!");
+    }
+}
